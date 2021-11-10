445ed9e220a86f9ec36c80144f73675cc330caa1
Chris Anderson <cva@pobox.com>
2021-11-10 15:08:18 -0600

    Update main.py
    Another version bump

--- hello_pipx/main.py
+++ hello_pipx/main.py
@@ -1,9 +1,8 @@
 import requests
 
-__version__ = "0.0.0.2"
+__version__ = "0.0.0.3"
 
 def main():
     print(f"Hello world! v{__version__}")
-    print("Change without bumping version")
     print()
     print(f"Your Public IP address is: {requests.get('https://ifconfig.me').text}")
