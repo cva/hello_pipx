e62b8166ed3efa89589d02327fc2d77567c2e0e7
Chris Anderson <cva@pobox.com>
2021-11-10 15:07:47 -0600

    Update main.py
    Changed main, but didn't bump version.

--- hello_pipx/main.py
+++ hello_pipx/main.py
@@ -4,5 +4,6 @@ __version__ = "0.0.0.2"
 
 def main():
     print(f"Hello world! v{__version__}")
+    print("Change without bumping version")
     print()
     print(f"Your Public IP address is: {requests.get('https://ifconfig.me').text}")
